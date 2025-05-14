7e8206848300d3d5dce97d30214049f5f981e464
Patrick Udo <mail@patrickudo.nl>
2025-05-14 11:34:48 +0200

    fix2

--- Covauto.Blazor/Program.cs
+++ Covauto.Blazor/Program.cs
@@ -6,7 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-// port
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5095/") });
+    // port
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5095/") });
 await builder.Build().RunAsync();

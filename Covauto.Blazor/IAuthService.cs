8a639251a26338db452f7cfb3e305af27ed8c907
Milan <87390214+Milann0@users.noreply.github.com>
2025-05-28 10:40:39 +0200

    navmenu (#9)
    * navmenu
    
    * delete file

---
+++ Covauto.Blazor/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Covauto.Blazor.Services
+{
+    public interface IAuthService
+    {
+        Task<bool> IsAuthenticatedAsync();
+        Task<(bool Success, string Message, string Username)> LoginAsync(string email, string password);
+        Task<bool> LogoutAsync();
+        Task<(bool Success, string Username, string Message)> GetUserInfoAsync();
+    }
+}

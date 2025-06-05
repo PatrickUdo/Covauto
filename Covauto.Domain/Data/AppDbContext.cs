9b3a6b7aed83193658cc1f5855a95e39633bb079
Milan <87390214+Milann0@users.noreply.github.com>
2025-06-05 12:34:01 +0200

    Feature/reseveringen (#14)
    * reseveringen
    
    * reserveringen
    
    ---------
    
    Co-authored-by: Patrick Udo <p.udo@udo-it.nl>

--- Covauto.Domain/Data/AppDbContext.cs
+++ Covauto.Domain/Data/AppDbContext.cs
@@ -10,7 +10,6 @@ namespace Covauto.Domain.Data
 
         public DbSet<Auto> Autos { get; set; }
         public DbSet<LeenAutoReservering> LeenAutoReserveringen { get; set; }
-        public DbSet<LeenAutoRit> LeenAutoRitten { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -25,17 +24,6 @@ namespace Covauto.Domain.Data
             modelBuilder.Entity<LeenAutoReservering>()
                 .HasOne(r => r.Auto)
                 .WithMany(a => a.Reserveringen)
-                .HasForeignKey(r => r.AutoId);
-
-            modelBuilder.Entity<LeenAutoRit>()
-                .HasOne(r => r.Werknemer)
-                .WithMany(u => u.LeenAutoRitten)
-                .HasForeignKey(r => r.WerknemerId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<LeenAutoRit>()
-                .HasOne(r => r.Auto)
-                .WithMany(a => a.Ritten)
                 .HasForeignKey(r => r.AutoId);
         }
 
---
+++ Covauto.Domain/Migrations/20250604094146_RmTable.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Covauto.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class RmTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeenAutoRitten");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Naam",
+                table: "Autos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Naam",
+                table: "Autos");
+
+            migrationBuilder.CreateTable(
+                name: "LeenAutoRitten",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AutoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    WerknemerId = table.Column<string>(type: "TEXT", nullable: false),
+                    AankomstTijd = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    KilometerstandBegin = table.Column<int>(type: "INTEGER", nullable: false),
+                    KilometerstandEind = table.Column<int>(type: "INTEGER", nullable: false),
+                    NaarAdres = table.Column<string>(type: "TEXT", nullable: false),
+                    VanAdres = table.Column<string>(type: "TEXT", nullable: false),
+                    VertrekTijd = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeenAutoRitten", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeenAutoRitten_AspNetUsers_WerknemerId",
+                        column: x => x.WerknemerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LeenAutoRitten_Autos_AutoId",
+                        column: x => x.AutoId,
+                        principalTable: "Autos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeenAutoRitten_AutoId",
+                table: "LeenAutoRitten",
+                column: "AutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeenAutoRitten_WerknemerId",
+                table: "LeenAutoRitten",
+                column: "WerknemerId");
+        }
+    }
+}
